ea35b55e9c010ebd8c1edd3be106caf436a3ef36
dietortoise25 <dietortoise25@gmail.com>
2025-05-03 21:24:57 +0800

    fixed image path

--- src/components/HomePage/ProductSection.tsx
+++ src/components/HomePage/ProductSection.tsx
@@ -4,25 +4,25 @@ const ProductsInfo = [
     {
         title: "CPO Charging",
         description: "Innovate your business with Autel Smart Charging Network and invest for the future success. We offer both air-cooled fast charging and liquid-cooled ultra-fast charging solutions that are compatible with most vehicle models, ensuring fast and smooth charging experience with multiple payment methods.",
-        image: "src/assets/Sessions/solution-card-1.png",
+        image: "/assets/Sessions/solution-card-1.png",
         link: "#",
     },
     {
         title: "Fleet Charging",
         description: "Innovate your business with Autel Smart Charging Network and invest for the future success. We offer both air-cooled fast charging and liquid-cooled ultra-fast charging solutions that are compatible with most vehicle models, ensuring fast and smooth charging experience with multiple payment methods.",
-        image: "src/assets/Sessions/solution-card-2.png",
+        image: "/assets/Sessions/solution-card-2.png",
         link: "#"
     },
     {
         title: "Destination Charging",
         description: "Autel offers one-stop solution for business owners with enhanced reliability and ensure higher yields. With customized consultance, reliable design, intelligent software and global service network, we ensure long-term worry free charging experience.",
-        image: "src/assets/Sessions/solution-card-3.png",
+        image: "/assets/Sessions/solution-card-3.png",
         link: "#"
     },
     {
         title: "Residential Charing",
         description: "We strive to create an ultimate and seamless charging experience for desired homes with sleek design, top-tier reliability and superior charging efficiency. Our smart home charging solution optimizes charging power, prioritizes safety, and intelligently integrates green energy for future possibilities.",
-        image: "src/assets/Sessions/solution-card-4.png",
+        image: "/assets/Sessions/solution-card-4.png",
         link: "#"
     }
 ]
